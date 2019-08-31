7bf6f34f6385db9bbaf93d44a5b4da824f47f098
sergio <pg11024@gmail.com>
2019-08-31 14:59:45 +0100

    add docker and dependencies

---
+++ docker/dynamodb/setup-dynamodb.sh
@@ -0,0 +1,25 @@
+#!/bin.bash
+
+# initializing dynamodb
+java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb &
+
+# will wait until the dynamodb is up to create tables
+while ! nc -z localhost 8000; do sleep 1; done
+
+# create message jornal table
+~/.local/bin/aws dynamodb create-table --table-name dynamo_message_journal \
+--attribute-definitions AttributeName=par,AttributeType=S AttributeName=num,AttributeType=N \
+--key-schema AttributeName=par,KeyType=HASH AttributeName=num,KeyType=RANGE \
+--provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+--endpoint-url http://localhost:8000
+
+# create snapchot table
+~/.local/bin/aws dynamodb create-table --table-name dynamo_message_snapshots \
+--attribute-definitions AttributeName=par,AttributeType=S AttributeName=seq,AttributeType=N AttributeName=ts,AttributeType=N \
+--key-schema AttributeName=par,KeyType=HASH AttributeName=seq,KeyType=RANGE \
+--local-secondary-indexes IndexName=ts-idx,KeySchema=["{AttributeName=par,KeyType=HASH}","{AttributeName=ts,KeyType=RANGE}"],Projection="{ProjectionType=ALL}" \
+--provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+--endpoint-url http://localhost:8000
+
+# keep the docker conteiner alive
+while true; do sleep 5; done
--- src/main/java/com/flutter/example/sgs/Main.java
+++ src/main/java/com/flutter/example/sgs/Main.java
@@ -1,6 +1,7 @@
-package com.flutter.example.sgs.node;
+package com.flutter.example.sgs;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
